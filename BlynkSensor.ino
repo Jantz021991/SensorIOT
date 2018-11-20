7fbdfbbd4921270d36916c2def57bbcc3425a496
Deepika Angelene Jantz <djantz@unomaha.edu>
2018-11-19 22:15:02 -0600

    Multiple sensors - connected to ESP8266 Arduino

--- BlynkSensor.ino
+++ BlynkSensor.ino
@@ -5,19 +5,26 @@
 #include <SimpleTimer.h>
 #include <Adafruit_Sensor.h>
 #include "DHT.h"
+#include <OneWire.h>
+#include <DallasTemperature.h> 
 
-// You should get Auth Token in the Blynk App.
-// Go to the Project Settings (nut icon).
+
+// Auth Token  is from the Blynk App.
 char auth[] = "bef3d1eb944e4e508e867e961d008e85";
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
-char ssid[] = "Inception";
-char pass[] = "dArKnIgHt@92922";
+char ssid[] = "***********";
+char pass[] = "************";
+int sensor = 0;
+int lastState = 0;
 
-#define DHTPIN 4         // What digital pin we're connected to
-
+#define DHTPIN 4         // Digital pin as connected to DHT22 sensor
+#define ONE_WIRE_BUS 2  // Digital pin as connected for DS18B20 sensor
 #define DHTTYPE DHT22  
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
 
 DHT dht(DHTPIN, DHTTYPE);
 SimpleTimer timer;
@@ -25,8 +32,13 @@ SimpleTimer timer;
 void sendSensor(){
   float h = dht.readHumidity();
   float t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
+  sensors.begin();    
+  sensors.requestTemperatures();                  // Polls the sensors.
+  sensors.setResolution(10);   
+  float c =  sensors.getTempCByIndex(0);   
+  
   int timeSinceLastRead = 0;
-  if (isnan(h) || isnan(t)) {
+  if (isnan(h) || isnan(t) || isnan(c) ) {
     Serial.println("Failed to read from DHT sensor!");
     return;
   }
@@ -34,12 +46,8 @@ void sendSensor(){
   
   Blynk.virtualWrite(V5, h);
   Blynk.virtualWrite(V6, t);
-
-  if (isnan(h) || isnan(t)) {
-      Serial.println("Failed to read from DHT sensor!");
-      timeSinceLastRead = 0;
-      return;
-    }
+  Blynk.virtualWrite(V1, c);
+  
   // SETUP the ALARM Trigger and Send EMAIL 
   // and PUSH Notification
   if(t > 28){
@@ -71,4 +79,23 @@ void setup(){
 void loop(){
   Blynk.run();
   timer.run();
+  
+  //Sensor to check the water Level 
+   sensor=analogRead(A0);
+  if (sensor < 300 && lastState == 0) {
+   Blynk.email("deeps.jantz@gmail.com", "Low Water Level", "Humidity over 60%");
+   Blynk.notify("Food Computer 1  Alert - Low Water Level!");
+   lastState = 1;
+   delay(1000);
+  }
+  else if (sensor < 300 && lastState == 1) {
+   delay(1000);
+   else {
+    //State
+    lastState = 0;
+    delay(1000);
+  }
+  
+  delay(100);
+  }
 }
